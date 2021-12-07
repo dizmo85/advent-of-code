6d40126b018da7168f20075d66aac31e9a8b70e6
dizmo85 <quantumswing@gmail.com>
2021-12-06 17:07:29 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using advent_of_code.day01;
+
+namespace advent_of_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventOfCodeDay01 day01 = new AdventOfCodeDay01();
+            int count = day01.CountIncreases();
+            System.Console.WriteLine(count);
+            System.Console.Read();
+        }
+    }
+}
---
+++ day01/AdventOfCodeDay01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace advent_of_code.day01
+{
+    class AdventOfCodeDay01
+    {
+        private List<int> ReadInputFromFile()
+        {
+            List<int> input = new List<int>();
+
+
+
+            return input;
+        }
+
+        public int CountIncreases()
+        {
+            int lastLine = int.MaxValue;
+            int increaseCount = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"D:\Documents\Projects\advent-of-code\day01\input.txt"))
+            {
+                int currentLine = Int32.Parse(line);
+                if (currentLine > lastLine)
+                {
+                    increaseCount++;
+                }
+                lastLine = currentLine;
+            }
+
+            return increaseCount;
+        }
+    }
+}
