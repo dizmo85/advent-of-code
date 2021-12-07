2ab48357d6514d7e3bcc0dcdd98af010b8be0908
Paul Stephens <pstephens@hearsaycorp.com>
2021-12-06 21:36:03 -0800

    Completed day 1 part 2

--- day01/AdventOfCodeDay01.cs
+++ day01/AdventOfCodeDay01.cs
@@ -1,29 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace advent_of_code.day01
 {
-    class AdventOfCodeDay01
+    class Day01
     {
-        private List<int> ReadInputFromFile()
-        {
-            List<int> input = new List<int>();
-
-
-
-            return input;
-        }
+        private string filePath = @"C:\Users\PaulStephens\projects\advent-of-code\day01\input.txt";
 
         public int CountIncreases()
         {
             int lastLine = int.MaxValue;
             int increaseCount = 0;
 
-            foreach (string line in System.IO.File.ReadLines(@"D:\Documents\Projects\advent-of-code\day01\input.txt"))
+            foreach (string line in File.ReadLines(filePath))
             {
                 int currentLine = Int32.Parse(line);
                 if (currentLine > lastLine)
@@ -31,6 +23,30 @@ namespace advent_of_code.day01
                     increaseCount++;
                 }
                 lastLine = currentLine;
+            }
+
+            return increaseCount;
+        }
+
+        public int CountSumIncreases()
+        {
+            int increaseCount = 0;
+            int lastSum = int.MaxValue;
+            List<int> inputs = new List<int>();
+
+            foreach (string line in File.ReadLines(filePath))
+            {
+                inputs.Add(Int32.Parse(line));
+            }
+
+            for (int i = 0; i <= inputs.Count - 3; i++)
+            {
+                int sum = inputs.Skip(i).Take(3).Sum();
+                if (sum > lastSum)
+                {
+                    increaseCount++;
+                }
+                lastSum = sum;
             }
 
             return increaseCount;
