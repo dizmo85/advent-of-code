23b1557bfbffc65719ad12f192c348305fda4c36
Paul Stephens <pstephens@hearsaycorp.com>
2021-12-15 20:03:55 -0800

    Finish day 05

--- day05/Day05.cs
+++ day05/Day05.cs
@@ -24,26 +24,37 @@ namespace advent_of_code.day05
                 //IEnumerable<int> x_line = Enumerable.Range(start[0] <= end[0] ? start[0] : end[0], Math.Abs(end[0] - start[0]) + 1);
                 //IEnumerable<int> y_line = Enumerable.Range(start[1] <= end[1] ? start[1] : end[1], Math.Abs(end[1] - start[1]) + 1);
 
-                List<int[]> lineSeg = new List<int[]>() { new int[] { start[0] <= end[0] ? start[0] : end[0], start[1] <= end[1] ? start[1] : end[1] } };
+                List<int[]> lineSeg = new List<int[]>() { new int[] { start[0], start[1] } };
 
-                List<int> x_line = new List<int>() { 1 };
-                List<int> y_line = new List<int>() { 1 };
-                for (int i = 1; i <= Math.Abs(end[0] - start[0]); i++)
+                //List<int> x_line = new List<int>() { 1 };
+                //List<int> y_line = new List<int>() { 1 };
+                int p = 1;
+                //for (int i = 1; i <= Math.Abs(end[0] - start[0]); i++)
+                while (true)
                 {
                     int[] point = new int[] { lineSeg[0][0], lineSeg[0][1] };
 
-                    if (start[0] != end[0])
-                        point[0] += i;
+                    if (start[0] < end[0])
+                        point[0] += p;
+                    else if (start[0] > end[0])
+                        point[0] -= p;
                     //x_line.Add(Convert.ToInt32(start[0] <= end[0] ? start[0] : end[0]) + i);
 
-                    if (start[1] != end[1])
-                        point[1] += i;
+                    if (start[1] < end[1])
+                        point[1] += p;
+                    if (start[1] > end[1])
+                        point[1] -= p;
                     //y_line.Add(Convert.ToInt32(start[1] <= end[1] ? start[1] : end[1]) + i);
                     lineSeg.Add(point);
+
+                    if (point.SequenceEqual(end))
+                        break;
+
+                    p++;
                 }
 
                 foreach (int[] i in lineSeg)
-                {   
+                {
                     string point = string.Format("{0},{1}", i[0], i[1]);
 
                     if (!ventPoints.ContainsKey(point))
